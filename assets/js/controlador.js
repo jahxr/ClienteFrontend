d0571a4dafafff16147d9edfa1f91aa04efaae80
jahxr <125504807+jahxr@users.noreply.github.com>
2023-05-04 17:07:46 +0200

    Add files via upload

--- assets/js/controlador.js
+++ assets/js/controlador.js
@@ -27,37 +27,53 @@ function login() {
     const emailUser = document.getElementById("emailLogin").value;
     const passUser = document.getElementById("passwordLogin").value;
 
-    var ban = false;
-
-    users.forEach(user => {
-        if (user.email.trim() === emailUser.trim()) {
-            if (user.password.trim() === passUser.trim()) {
-                window.location = 'home.html';
-                ban = true;
-                return;
-            } else {
-                alert("password incorrect");
-                return;
-            }
-        }
-    });
-
-    if (!ban) {
-        alert("User not exist")
+    let _datos = {
+        email : emailUser,
+        password: passUser, 
     }
 
+    fetch('https://backend-jahxr.vercel.app/client/login', {
+        method: "POST",
+        body: JSON.stringify(_datos),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json())
+    .then(json => {
+        window.location = 'home.html';
+        console.log(json)})
+    .catch(err => {
+        alert("password incorrect");
+        return;
+    });
 }
 
 
 function sign() {
     const emailUser = document.getElementById("emailSignup").value;
-    const passUser = document.getElementById("numberSignup").value;
+    const passUser = document.getElementById("passwordLogin").value;
     const nameUser= document.getElementById("usernameSignup").value;
     const numberUser= document.getElementById("numberSignup").value;
-     user.forEach(user=>{
-        if((user.email.trim() === emailUser.trim()) ){
-            alert("User alreay exist")
-        }
-     })
+
+    let _datos = {
+        email : emailUser,
+        phone: numberUser,
+        username: nameUser,
+        password: passUser, 
+    }
+
+    fetch('https://backend-jahxr.vercel.app/client/signin', {
+        method: "POST",
+        body: JSON.stringify(_datos),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json())
+    .then(json => {
+        window.location = 'home.html';
+        console.log(json)
+    })
+    .catch(err => {
+        alert("User alreay exist")
+        return;
+    });
 }
 
